f05a4d8e6579b85d9afd3dba9de545ecf1e116e0
NalKame <nalkame.nalkame@gmail.com>
2023-03-22 00:29:35 +0900

    analytics追加

---
+++ docs/common/analytics.js
@@ -0,0 +1,5 @@
+document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-E66TFNPQ6Z"></script>');
+window.dataLayer = window.dataLayer || [];
+function gtag(){dataLayer.push(arguments);}
+gtag('js', new Date());
+gtag('config', 'G-E66TFNPQ6Z');
