875873f9b4cab35b88c6a5acf7cb2f53995aa1c5
NalKame <nalkame.nalkame@gmail.com>
2023-03-22 00:44:44 +0900

    analyticsを本番のみ差し込みに変更

--- docs/common/analytics.js
+++ docs/common/analytics.js
@@ -1,5 +1,9 @@
-document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-E66TFNPQ6Z"></script>');
-window.dataLayer = window.dataLayer || [];
-function gtag(){dataLayer.push(arguments);}
-gtag('js', new Date());
-gtag('config', 'G-E66TFNPQ6Z');
+//本番環境のみ差し込み
+if(location.hostname==='nalkame.github.io'){
+    //googleアナリティクス
+    document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-E66TFNPQ6Z"></script>');
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'G-E66TFNPQ6Z');
+}
