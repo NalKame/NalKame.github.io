287473b279adacba8bb1cc53240aee0753f7ca46
NalKame <nalkame.nalkame@gmail.com>
2023-03-25 16:21:38 +0900

    「Google Analytics を設定した」公開と、その他更新

--- docs/common/analytics.js
+++ docs/common/analytics.js
@@ -1,7 +1,7 @@
 //本番環境のみ差し込み
 if(location.hostname==='nalkame.github.io'){
     //googleアナリティクス
-    document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-E66TFNPQ6Z"></script>');
+    document.head.insertAdjacentHTML('beforeend','<script async src="https://www.googletagmanager.com/gtag/js?id=G-E66TFNPQ6Z"></script>');
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
