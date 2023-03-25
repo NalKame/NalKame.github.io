14429042d1506583316b9b9c9484da32b917a774
NalKame <nalkame.nalkame@gmail.com>
2023-03-25 19:42:28 +0900

    analytics.js条件文戻す

--- docs/common/analytics.js
+++ docs/common/analytics.js
@@ -1,5 +1,5 @@
 //本番環境のみ差し込み
-if(location.hostname==='nalkame.github.io'||true){
+if(location.hostname==='nalkame.github.io'){
     //googleアナリティクス
     document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-E66TFNPQ6Z"></script>');
     window.dataLayer = window.dataLayer || [];
