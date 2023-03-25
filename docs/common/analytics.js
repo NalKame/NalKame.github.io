dd732ecfbcbb97e5dee68582efde0843da342103
NalKame <nalkame.nalkame@gmail.com>
2023-03-25 20:05:13 +0900

    読み込み方法変更

--- docs/common/analytics.js
+++ docs/common/analytics.js
@@ -1,7 +1,12 @@
 //本番環境のみ差し込み
 if(location.hostname==='nalkame.github.io'){
     //googleアナリティクス
-    document.write('<script async src="https://www.googletagmanager.com/gtag/js?id=G-E66TFNPQ6Z"></script>');
+    {
+        const script_element = document.createElement('script');
+        script_element.src='https://www.googletagmanager.com/gtag/js?id=G-E66TFNPQ6Z';
+        script_element.setAttribute('async','');
+        document.head.appendChild(script_element);
+    }
     window.dataLayer = window.dataLayer || [];
     function gtag(){dataLayer.push(arguments);}
     gtag('js', new Date());
