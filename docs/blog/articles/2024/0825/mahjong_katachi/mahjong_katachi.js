1f4d3e7c83ac06ee71a1e50226192fa7312b095a
Nalkame <nalkame.nalkame@gmail.com>
2024-08-25 19:41:50 +0900

    「麻雀 同種数牌の組み合わせを調べる。」

---
+++ docs/blog/articles/2024/0825/mahjong_katachi/mahjong_katachi.js
@@ -0,0 +1,42 @@
+function mahjong_katachi(num){
+    if(num<1)throw new Error();
+    if(num===1)return "123456789".split("").map(t=>[parseInt(t)]);
+    const prevList=mahjong_katachi(num-1);
+    const results=[];
+    for(const prev of prevList){
+        const max=prev[prev.length-1];
+        const countMax=prev.filter(t=>t===max).length;
+        const begin=countMax<4?max:max+1;
+        for(let i=begin;i<=9;i++){
+            const list=[];
+            list.push(...prev);
+            list.push(i);
+            results.push(list);
+        }
+    }
+    return results;
+}
+
+function builMahjongKatachiDOMElements(pBegin,pEnd){
+    const begin=pBegin;
+    const end=pEnd===undefined?pBegin:pEnd;
+    const elResults=document.getElementById("results");
+    for(let i=begin;i<=end;i++){
+        const elDeitals=document.createElement("details");
+        elDeitals.classList.add("accordion");
+        const elSummary=document.createElement("summary");
+        elSummary.classList.add("accordion__summary");
+        elSummary.innerText=i+"枚";
+        elDeitals.append(elSummary);
+        const elAccordion=document.createElement("div");
+        elAccordion.classList.add("accordion__detail");
+        const results=mahjong_katachi(i);
+        for(const res of results){
+            const el=document.createElement("div");
+            el.innerText=res;
+            elAccordion.append(el);
+        }
+        elDeitals.append(elAccordion);
+        elResults.append(elDeitals);
+    }
+}
